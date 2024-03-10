d3c4011e97579fc491788207fa9bad7c2de3ed43
MeghanaKancherla <69040277+MeghanaKancherla@users.noreply.github.com>
2024-03-09 20:13:43 -0500

    Update prompt.py

--- GentPool/gentpool/pool/scholar/prompt.py
+++ GentPool/gentpool/pool/scholar/prompt.py
@@ -1,17 +1,3 @@
 ### Define your custom prompt here. Check prebuilts in gentopia.prompt :)###
 from gentopia.prompt import *
 from gentopia import PromptTemplate
-from tool import GoogleSearch
-
-class GoogleSearchPrompt(PromptTemplate):
-   
-    def handle_search_query(self, query):
-        search_tool = GoogleSearch()
-        search_results = search_tool._run(query)
-        return search_results
-
-    def generate_response(self, query):
-        if 'search' in query:
-            return self.handle_search_query(query)
-
-
