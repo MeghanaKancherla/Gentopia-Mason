ab8e652bcbc997547691987522821de616c96251
MeghanaKancherla <69040277+MeghanaKancherla@users.noreply.github.com>
2024-03-09 19:39:46 -0500

    Adding GoogleSearch in prompt.py

--- GentPool/gentpool/pool/scholar/prompt.py
+++ GentPool/gentpool/pool/scholar/prompt.py
@@ -1,3 +1,16 @@
 ### Define your custom prompt here. Check prebuilts in gentopia.prompt :)###
 from gentopia.prompt import *
 from gentopia import PromptTemplate
+
+class GoogleSearchPrompt(PromptTemplate):
+   
+    def handle_search_query(self, query):
+        search_tool = GoogleSearch()
+        search_results = search_tool._run(query)
+        return search_results
+
+    def generate_response(self, query):
+        if 'search' in query:
+            return self.handle_search_query(query)
+
+
